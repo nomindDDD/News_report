43640cc51e2b119b55176a22f5353436d3decf10
dang <dangpeizheng@163.com>
2019-06-10 22:15:43 +0800

    news_web 添加配置类 将配置类加载到app中

--- news_home.py
+++ news_home.py
@@ -2,10 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Config():
+    # 配置
+    DEBUG = True
+
+# 将配置加载到app中
+app.config.from_object(Config)
+
 @app.route("/")
 def index():
 
     return "新闻主页"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
